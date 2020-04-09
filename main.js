955d55c7e3c046be4b3ce5e4be1d4eb24a3ed2cc
RudnickiPawel <rudnicki.pawel94@gmail.com>
2020-04-09 21:46:06 +0200

    some twiking with hamburger and menu

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  
+  //show nav menu
+  if($('.nav__hamburger-icon input').get(0).checked)
+    $('.nav__hamburger-menu').css('transform', 'none');
+    $('.nav__hamburger-icon input').on( "click", function() {
+      if(this.checked)
+        $('.nav__hamburger-menu').css('transform', 'none');
+      else
+        $('.nav__hamburger-menu').css('transform', 'translate(-1000%, 0)');
+    });
 })
